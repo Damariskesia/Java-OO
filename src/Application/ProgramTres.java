f6fab0c9dc6a28dfbcba1541709331833e2d2d29
Dama <damariskvaz@hotmail.com>
2024-06-30 00:23:50 -0300

    exercicios Java OO iniciais - até a aula 73

---
+++ src/Application/ProgramTres.java
@@ -0,0 +1,24 @@
+package Application;
+
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class ProgramTres {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("AREA = " + rectangle.area());
+        System.out.println("PERIMETER = " + rectangle.perimeter());
+        System.out.println("DIAGONAL = " + rectangle.diagonal());
+
+        sc.close();
+    }
+
+}
