19fe2133c250bf38c1bc6773b79bfbca6859d5d3
Dama <damariskvaz@hotmail.com>
2024-07-03 22:10:38 -0300

    Composições parte 1

---
+++ src/Application/ProgramVinte.java
@@ -0,0 +1,13 @@
+package Application;
+
+import java.util.Scanner;
+
+public class ProgramVinte {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
+}
